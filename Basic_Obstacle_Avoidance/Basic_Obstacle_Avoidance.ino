c8634297604f6f03064c761848b7a9b3f9fd5840
Nathan Boyd <nboyd2226@gmail.com>
2017-05-24 16:54:08 -0700

    Add files via upload

--- Basic_Obstacle_Avoidance/Basic_Obstacle_Avoidance.ino
+++ Basic_Obstacle_Avoidance/Basic_Obstacle_Avoidance.ino
@@ -21,31 +21,45 @@ void setup()
 
 void loop() 
 {
- 
+  Serial.print("middle");
+ Serial.println(getSonarDistance(middle));
   //if something is in the way of the robot
-  if(getSonarDistance(left) < dangerZone || getSonarDistance(right) < dangerZone || getSonarDistance(middle) < dangerZone)
+  if(getSonarDistance(left) < dangerZone || getSonarDistance(right) < dangerZone)
+  {
+    if(getSonarDistance(left) > getSonarDistance(right) && (getSonarDistance(right) < 100 || getSonarDistance(left) < 100))//something is closer on the right side
+    {
+      frontSteer.write(90-turn); //turn left
+      //frontSteer.write(90+turn);
+      //delay(2000);
+    }
+    else if(getSonarDistance(left) < getSonarDistance(right) && (getSonarDistance(left) < 100 || getSonarDistance(right) < 100) )// something closer on the left side
+    {
+      frontSteer.write(90+turn);
+      //frontSteer.write(90-turn);
+     // delay(2000);
+    }
+   
+  }
+  else if(getSonarDistance(middle) < dangerZone)
   {
     if(getSonarDistance(left) > getSonarDistance(right))//something is closer on the right side
     {
       frontSteer.write(90-turn); //turn left
-      delay(2000);
-      //frontSteer.write(90+turn);
-      //delay(2000);
     }
-    else if(getSonarDistance(left) < getSonarDistance(right))// something closer on the left side
+    else if(getSonarDistance(left) < getSonarDistance(right) )// something closer on the left side
     {
       frontSteer.write(90+turn);
-      delay(2000);
-      //frontSteer.write(90-turn);
-     // delay(2000);
     }
-    frontSteer.write(90);
-  }
+  } 
+  else
+    {
+      frontSteer.write(90);
+    }
   
-  drive.write(105);
+  drive.write(102);
 }
 
 float getSonarDistance(int sonarPin)
 {
-  float distance = (getPing(sonarPin)*.034)/2;
+  return (getPing(sonarPin)*.034)/2;
 }
